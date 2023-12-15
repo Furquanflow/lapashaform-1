3f7aff48b6ee470ab418bff929fd5f248182443e
Furquanflow <141862606+Furquanflow@users.noreply.github.com>
2023-12-15 19:59:54 +0500

    Update LapashaLoungeAndGrill.jsx

--- client/src/admin panel/pages/LapashaLoungeAndGrill.jsx
+++ client/src/admin panel/pages/LapashaLoungeAndGrill.jsx
@@ -22,59 +22,12 @@ const LapashaLoungeAndGrill = ({ loungeGrillEditFunc }) => {
         console.error("Error getting data:", error);
       });
   };
-
-  //update function
-
-  // Assume you have a function to get the authentication token
-  // const getAuthToken = () => {
-  //   // Replace 'your_token_here' with the actual token retrieval logic
-  //   return 'your_token_here';
-  // };
-
-  // const updateFormData = async (recordId, newData) => {
-  //   try {
-  //     // const token = getAuthToken();
-  //     const response = await axios.put(
-  //       `${baseUrl}/updateFormData/${recordId}`,
-  //       {
-  //         newData
-  //       },
-  //       {
-  //         headers: {
-  //           Authorization: `Bearer ${token}`,
-  //           'Content-Type': 'application/json'
-  //         }
-  //       }
-  //     );
-
-  //     if (response.status === 200) {
-  //       console.log('Update successful');
-  //       // Handle any additional logic after a successful update
-  //     } else {
-  //       console.error('Update failed');
-  //       // Handle errors or provide user feedback
-  //     }
-  //   } catch (error) {
-  //     console.error('Error updating data:', error);
-  //     // Handle errors or provide user feedback
-  //   }
-  // };
-
-  // Example usage
-  // const recordIdToUpdate = 'your_record_id_here';
-  // const updatedData = {
-  //   fNamePerInfo: 'Updated First Name',
-  //   lnamePerInfo: 'Updated Last Name',
-  //   statePerInfo: 'Updated State',
-  // };
-
-  // updateFormData(recordIdToUpdate, updatedData);
-
   React.useEffect(() => {
     getFormData();
   }, []);
   return (
     <LapashaFormData
+    
       lapashaData={adminData}
       title={"Lapasha lounge And Grill"}
       empolymentFunc={e => loungeGrillEditFunc(e, 0)}
