7b0299e22a0465834188f74a9b7a32a9dc4a35e8
Furquanflow <141862606+Furquanflow@users.noreply.github.com>
2023-12-18 10:06:24 +0500

    Update authMiddleware.js
    Comment Middle ware

--- server/middle ware/authMiddleware.js
+++ server/middle ware/authMiddleware.js
@@ -1,12 +1,12 @@
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
-module.exports.authenticateToken = (req, res, next) => {
-  const token = req.header('Authorization');
-  if (!token) return res.sendStatus(401);
+// module.exports.authenticateToken = (req, res, next) => {
+//   const token = req.header('Authorization');
+//   if (!token) return res.sendStatus(401);
 
-  jwt.verify(token, 'secret123', (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = user;
-    next();
-  });
-};
+//   jwt.verify(token, 'secret123', (err, user) => {
+//     if (err) return res.sendStatus(403);
+//     req.user = user;
+//     next();
+//   });
+// };
