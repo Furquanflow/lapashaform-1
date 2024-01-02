74089df1d01245698616fcb9f54568286b442b3d
Furquanflow <furqan.rahim@flowtechnologies.io>
2024-01-03 03:14:28 +0500

    Add Cors

--- server/index.js
+++ server/index.js
@@ -8,18 +8,18 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://3.144.216.200:8000",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true
-  })
-);
-// app.use(cors());
+// app.use(
+//   cors({
+//     origin: "http://3.144.216.200:8000",
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     credentials: true
+//   })
+// );
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 8000;
-const IP_ADDRESS = process.env.IP_ADDRESS || '0.0.0.0';
+// const IP_ADDRESS = process.env.IP_ADDRESS || '0.0.0.0';
 app.use(routes);
 
 mongoose
@@ -27,12 +27,12 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.log(err));
 
-const _dirName = path.dirname("");
-const buildPath = path.resolve(_dirName, "../client/build");
-app.use(express.static(buildPath));
-app.get("*/", (req, res) => {
-  const indexPath = path.resolve(buildPath, "index.html");
-  res.sendFile(indexPath);
-});
+// const _dirName = path.dirname("");
+// const buildPath = path.resolve(_dirName, "../client/build");
+// app.use(express.static(buildPath));
+// app.get("*/", (req, res) => {
+//   const indexPath = path.resolve(buildPath, "index.html");
+//   res.sendFile(indexPath);
+// });
 
 app.listen(PORT, () => console.log(`Port Running at ${PORT}`));
