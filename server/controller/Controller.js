f8a5f908c1bc898e81c17c0190715284ff6c6637
Furquanflow <furqan.rahim@flowtechnologies.io>
2024-01-26 00:54:50 +0500

    Remove Controller

--- server/controller/Controller.js
+++ server/controller/Controller.js
@@ -331,68 +331,6 @@ module.exports.updateSaveNaraCafeData = async (req, res) => {
   }
 };
 
-// const transporter = nodemailer.createTransport({
-//   host: "flowtechnologies.io",
-//   port: 465,
-//   secure: true,
-//   auth: {
-//     user: "furqan.rahim@flowtechnologies.io",
-//     pass: "Furqan@123@@@"
-//   }
-// });
-
-// module.exports.postPdf = async (req, res) => {
-//   const formData = req.body.data;
-//   console.log("Working");
-//   try {
-//     const browser = await puppeteer.launch({ headless: "new" });
-//     console.log("Working");
-//     const page = await browser.newPage();
-//     await page.goto(`${baseUrl}/eligibilityverificationview`);
-//     const pdfBuffer = await page.pdf({ format: "A4" });
-//     const pdfPath = path.join(__dirname, "generated.pdf");
-//     fs.writeFileSync(pdfPath, pdfBuffer);
-//     await browser.close();
-//     const emailAddresses = ["rammal.ali003@gmail.com"];
-//     const attachments = [{ filename: "generated.pdf", content: pdfBuffer }];
-//     emailAddresses.forEach(email => {
-//       const mailOptions = {
-//         from: "furqan.rahim@flowtechnologies.io",
-//         to: email,
-//         subject: "PDF Attachment",
-//         text: "Attached is the PDF you requested.",
-//         attachments
-//       };
-//       const operation = retry.operation({
-//         retries: 3,
-//         factor: 2,
-//         minTimeout: 1000,
-//         maxTimeout: 30000
-//       });
-//       operation.attempt(currentAttempt => {
-//         transporter.sendMail(mailOptions, (error, info) => {
-//           if (operation.retry(error)) {
-//             console.error("Email not sent, retrying...", currentAttempt);
-//             return;
-//           }
-//           if (error) {
-//             console.error("Email not sent:", error);
-//           } else {
-//             console.log("Email sent:", info.response);
-//           }
-//         });
-//       });
-//     });
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Methods", "POST");
-//     res.header("Access-Control-Allow-Headers", "Content-Type");
-//     res.json({ pdfPath: "/download-pdf" });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).send("Internal Server Error");
-//   }
-// };
-
 const transporter = nodemailer.createTransport({
   host: "flowtechnologies.io",
   port: 465,
@@ -406,45 +344,45 @@ const transporter = nodemailer.createTransport({
 module.exports.postPdf = async (req, res) => {
   const formData = req.body.data;
   console.log("Working");
-
   try {
     const browser = await puppeteer.launch({ headless: "new" });
     console.log("Working");
     const page = await browser.newPage();
     await page.goto(`${baseUrl}/eligibilityverificationview`);
-
-    // Wait for window.print to finish
-    await page.evaluate(() => {
-      return new Promise((resolve) => {
-        window.onafterprint = resolve;
-        window.print();
-      });
-    });
-
-    // Wait for a short duration (adjust as needed) to ensure PDF is generated
-    await page.waitForTimeout(3000);
-
     const pdfBuffer = await page.pdf({ format: "A4" });
     const pdfPath = path.join(__dirname, "generated.pdf");
     fs.writeFileSync(pdfPath, pdfBuffer);
-
     await browser.close();
-
     const emailAddresses = ["rammal.ali003@gmail.com"];
     const attachments = [{ filename: "generated.pdf", content: pdfBuffer }];
-
-    // Send email with the generated PDF as an attachment
-    const mailOptions = {
-      from: "furqan.rahim@flowtechnologies.io",
-      to: emailAddresses.join(', '), // Join multiple email addresses with comma
-      subject: "PDF Attachment",
-      text: "Attached is the PDF you requested.",
-      attachments
-    };
-
-    await transporter.sendMail(mailOptions);
-    console.log("Email sent successfully!");
-
+    emailAddresses.forEach(email => {
+      const mailOptions = {
+        from: "furqan.rahim@flowtechnologies.io",
+        to: email,
+        subject: "PDF Attachment",
+        text: "Attached is the PDF you requested.",
+        attachments
+      };
+      const operation = retry.operation({
+        retries: 3,
+        factor: 2,
+        minTimeout: 1000,
+        maxTimeout: 30000
+      });
+      operation.attempt(currentAttempt => {
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (operation.retry(error)) {
+            console.error("Email not sent, retrying...", currentAttempt);
+            return;
+          }
+          if (error) {
+            console.error("Email not sent:", error);
+          } else {
+            console.log("Email sent:", info.response);
+          }
+        });
+      });
+    });
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "POST");
     res.header("Access-Control-Allow-Headers", "Content-Type");
@@ -454,146 +392,6 @@ module.exports.postPdf = async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 };
-
-
-
-
-
-//Employeer/Manager Pdf
-// module.exports.postEmployerPdf = async (req, res) => {
-//   const formData = req.body.data;
-//   console.log("Working");
-//   try {
-//     const browser = await puppeteer.launch({ headless: "new" });
-//     console.log("Working");
-//     const page = await browser.newPage();
-//     await page.goto(`${baseUrl}/eligibilityverificationview`);
-//     await page.waitForTimeout(28000);
-//     const pdfBuffer = await page.pdf({ format: "A4" });
-//     const pdfPath = path.join(__dirname, "generated.pdf");
-//     fs.writeFileSync(pdfPath, pdfBuffer);
-//     await browser.close();
-//     const emailAddresses = [
-//       "thefurquanrahim@gmail.com",
-//       "furquan.rahim124@gmail.com",
-//       "thefurqanrahim@gmail.com"
-//     ];
-//     const attachments = [{ filename: "generated.pdf", content: pdfBuffer }];
-//     emailAddresses.forEach(email => {
-//       const mailOptions = {
-//         from: "furqan.rahim@flowtechnologies.io",
-//         to: email,
-//         subject: "PDF Attachment",
-//         text: "Attached is the PDF you requested.",
-//         attachments
-//       };
-//       const operation = retry.operation({
-//         retries: 3,
-//         factor: 2,
-//         minTimeout: 1000,
-//         maxTimeout: 30000
-//       });
-//       operation.attempt(currentAttempt => {
-//         transporter.sendMail(mailOptions, (error, info) => {
-//           if (operation.retry(error)) {
-//             console.error("Email not sent, retrying...", currentAttempt);
-//             return;
-//           }
-//           if (error) {
-//             console.error("Email not sent:", error);
-//           } else {
-//             console.log("Email sent:", info.response);
-//           }
-//         });
-//       });
-//     });
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Methods", "POST");
-//     res.header("Access-Control-Allow-Headers", "Content-Type");
-//     res.json({ pdfPath: "/download-pdf" });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).send("Internal Server Error");
-//   }
-// };
-
-
-// module.exports.postPdf = async (req, res) => {
-//   const formData = req.body.data;
-//   console.log("Working");
-
-//   try {
-//     const browser = await puppeteer.launch({ headless: "new" });
-//     console.log("Working");
-//     const page = await browser.newPage();
-//     await page.goto(`${baseUrl}/eligibilityverificationview`);
-
-//     // Wait for navigation to complete (if applicable)
-//     // await Promise.all([
-//     //   page.waitForNavigation(),
-//     //   page.click('yourButtonSelector') // Replace with the actual selector or action triggering navigation
-//     // ]);
-
-//     // Capture a screenshot before waiting for the selector
-//     await page.screenshot({ path: 'debug-screenshot-before.png' });
-
-//     // Wait for the element with class 'new-data' to appear (replace with your actual selector)
-//     console.log("Waiting for selector to appear...");
-//     await page.waitForSelector('.new-data', { timeout: 60000 });
-//     console.log("Selector found!");
-
-//     // Capture another screenshot after waiting for the selector
-//     await page.screenshot({ path: 'debug-screenshot-after.png' });
-
-//     const pdfBuffer = await page.pdf({ format: "A4" });
-//     const pdfPath = path.join(__dirname, "generated.pdf");
-//     fs.writeFileSync(pdfPath, pdfBuffer);
-
-//     await browser.close();
-
-//     const emailAddresses = ["rammal.ali003@gmail.com"];
-//     const attachments = [{ filename: "generated.pdf", content: pdfBuffer }];
-
-//     emailAddresses.forEach(email => {
-//       const mailOptions = {
-//         from: "furqan.rahim@flowtechnologies.io",
-//         to: email,
-//         subject: "PDF Attachment",
-//         text: "Attached is the PDF you requested.",
-//         attachments
-//       };
-
-//       const operation = retry.operation({
-//         retries: 3,
-//         factor: 2,
-//         minTimeout: 1000,
-//         maxTimeout: 30000
-//       });
-
-//       operation.attempt(currentAttempt => {
-//         transporter.sendMail(mailOptions, (error, info) => {
-//           if (operation.retry(error)) {
-//             console.error("Email not sent, retrying...", currentAttempt);
-//             return;
-//           }
-//           if (error) {
-//             console.error("Email not sent:", error);
-//           } else {
-//             console.log("Email sent:", info.response);
-//           }
-//         });
-//       });
-//     });
-
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Methods", "POST");
-//     res.header("Access-Control-Allow-Headers", "Content-Type");
-//     res.json({ pdfPath: "/download-pdf" });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).send("Internal Server Error");
-//   }
-// };
 
 module.exports.getPdf = async (req, res) => {
   const pdfPath = path.join(__dirname, "generated.pdf");
